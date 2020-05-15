e31f0f655dee18655739d866723ac5710e6fe8f9
Ivan <ceaicovschi.ivan@yahoo.com>
2020-05-15 13:16:43 +0200

    added back to menu button while in game

--- app/src/main/java/dev/esgi/quiveutgagnerdesmillions/Game.java
+++ app/src/main/java/dev/esgi/quiveutgagnerdesmillions/Game.java
@@ -1,13 +1,17 @@
 package dev.esgi.quiveutgagnerdesmillions;
 
 import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.SystemClock;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Chronometer;
 import android.widget.TextView;
 
@@ -16,6 +20,7 @@ public class Game extends AppCompatActivity {
     private int index = TIMER_TIME;
     private Chronometer timer;
     private TextView question;
+    private Button back_to;
 
     @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
@@ -25,6 +30,7 @@ public class Game extends AppCompatActivity {
 
         timer = findViewById(R.id.chrono);
         question = findViewById(R.id.q);
+        back_to = findViewById(R.id.back_to_menu_warning);
 
 
         timer.start();
@@ -44,6 +50,30 @@ public class Game extends AppCompatActivity {
                     startActivity(i);
                     finish();
                 }
+            }
+        });
+
+        back_to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AlertDialog.Builder(Game.this)
+                        .setTitle(getResources().getString(R.string.realy_quit))
+                        .setPositiveButton(getResources().getString(R.string.pop_yes), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                Intent j = new Intent(Game.this, MainActivity.class);
+                                startActivity(j);
+                                finish();
+                            }
+                        })
+                        .setNegativeButton(getResources().getString(R.string.pop_no), new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+
+                            }
+                        })
+                        .setCancelable(false)
+                        .show();
             }
         });
 
